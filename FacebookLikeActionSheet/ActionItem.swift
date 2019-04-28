fad3434d6b5ea99c6f87166eec58d2ee72ae01e9
Omer Elimelech <omer.tkd@gmail.com>
2019-04-29 01:01:50 +0300

    .

---
+++ FacebookLikeActionSheet/ActionItem.swift
@@ -0,0 +1,19 @@
+//
+//  ActionItem.swift
+//  FacebookLikeActionSheet
+//
+//  Created by Omer Elimelech on 4/29/19.
+//  Copyright © 2019 Omer Elimelech. All rights reserved.
+//
+
+import Foundation
+
+
+struct ActionItem {
+    var iconName: String?
+    var titleString: String?
+    var expString: String?
+    var target: Any?
+    var selector: Selector?
+    
+}
