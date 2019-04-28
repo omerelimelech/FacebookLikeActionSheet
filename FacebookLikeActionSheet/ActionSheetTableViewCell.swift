2e2787530326010a081362ffb6df532b738821c9
Omer Elimelech <omer.tkd@gmail.com>
2019-04-29 00:52:09 +0300

    first commit

---
+++ FacebookLikeActionSheet/ActionSheetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ActionSheetTableViewCell.swift
+//  FacebookLikeActionSheet
+//
+//  Created by Omer Elimelech on 4/28/19.
+//  Copyright © 2019 Omer Elimelech. All rights reserved.
+//
+
+import UIKit
+
+class ActionSheetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ FacebookLikeActionSheet/ActionSheetViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ActionSheetViewController.swift
+//  FacebookLikeActionSheet
+//
+//  Created by Omer Elimelech on 4/28/19.
+//  Copyright © 2019 Omer Elimelech. All rights reserved.
+//
+
+import Foundation
